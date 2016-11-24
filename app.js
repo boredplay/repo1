741653769161875fbc88bae3ebce3d589f987049
Bruno Seguer <bseguer@tekgenesis.com>
2016-11-24 17:35:32 -0300

    commit 4

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("Starting app... __dirname:", __dirname);
+
+var steamApi = require("./libs/steamApi");
+// var steamApi = new steamApiClass();
+steamApi("lala", function(p){
+	console.log(p)
+});
---
+++ libs/steamApi.js
@@ -0,0 +1,20 @@
+var http = require("http");
+module.exports = function(q, cb){
+        var body = "";
+        http.get("http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002?appid=730&steamid=76561198237693572&key=D3AB52863B9F0893D06D2C5777624008", 
+                function(req){
+                        req.on("data", function(d){
+                                body = body+d.toString();
+                        });
+
+                        req.on("end", function(){
+                                var result = JSON.parse(body);
+                                // var imgs = result.responseData.results;
+                                // var html = "";
+                                // for(var x=0; x<imgs.length; x++){
+                                //         html = html+"<img src=\""+imgs[x].unescapedUrl+"\" />" ;
+                                // }//end for
+                                cb(result);
+                        });
+                });
+};
