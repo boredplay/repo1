d2431c105757a548aa383e21acf8a95813bf5fa9
Bruno Seguer <bseguer@tekgenesis.com>
2016-11-24 17:52:07 -0300

    steamapi

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 console.log("Starting app... __dirname:", __dirname);
 
-var steamApi = require("./libs/steamApi");
-// var steamApi = new steamApiClass();
-steamApi("lala", function(p){
-	console.log(p)
-});
+var steamApiClass = require("./libs/steamApi");
+var steamApi = new steamApiClass();
+
+
+
+steamApi.getUserStatsForGame(function(p){console.log(p)});
--- libs/steamApi.js
+++ libs/steamApi.js
@@ -1,8 +1,9 @@
 var http = require("http");
-module.exports = function(q, cb){
-        var body = "";
-        http.get("http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002?appid=730&steamid=76561198237693572&key=D3AB52863B9F0893D06D2C5777624008", 
+module.exports = function(){
+        this. getUserStatsForGame = function(cb){
+                http.get("http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002?appid=730&steamid=76561198237693572&key=D3AB52863B9F0893D06D2C5777624008", 
                 function(req){
+                        body = "";
                         req.on("data", function(d){
                                 body = body+d.toString();
                         });
@@ -17,4 +18,5 @@ module.exports = function(q, cb){
                                 cb(result);
                         });
                 });
+        }
 };
