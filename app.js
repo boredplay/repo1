b545d0fc1eae8d288d09a6d69857b7a55b35fb44
bored <brunoseguer@hotmail.com>
2016-11-26 02:33:40 -0300

    edu1

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 console.log("Starting app... __dirname:", __dirname);
 
+var http = require('http');
 var steamApiClass = require("./libs/steamApi");
 var steamApi = new steamApiClass();
 
-steamApi.getUserStatsForGame(function(p){console.log(p)});
+http.createServer(function(req, res){
+	console.log(steamApi);
+}).listen(3000);
+// steamApi.getUserStatsForGame(function(p){console.log(p)});
+//video6 51min
+
