fbe2b277a736eed6549f7e4df61d01e252ad133c
bored <brunoseguer@hotmail.com>
2017-05-10 03:48:56 -0300

    url with vars

--- libs/steamApi.js
+++ libs/steamApi.js
@@ -1,7 +1,13 @@
 var http = require("http");
+
+const appId = 730;
+const steamId = 76561198237693572;
+const key = 'D3AB52863B9F0893D06D2C5777624008';
+const apiVersion = 'v0002';
+
 module.exports = function(){
         this.getUserStatsForGame = function(cb){
-                http.get("http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002?appid=730&steamid=76561198237693572&key=D3AB52863B9F0893D06D2C5777624008", 
+                http.get(`http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/${apiVersion}?appid=${appId}&steamid=${steamId}&key=${key}`,
                 function(req){
                         body = "";
                         req.on("data", function(d){
